b62b6cd9255ee5510af611b81a5bf244be41c305
Kunal Indoliya <kunalindoliya@gmail.com>
2019-04-11 11:11:52 +0530

    Stores data into files.

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,16 @@
-const products=[];
+const Product=require('../models/product');
 exports.getAddProduct=(req,res,next) =>{
     res.render('add-product',{pageTitle:'Add Product',path:'/admin/addproduct'});
 };
 
 exports.postAddProduct=(req,res,next) =>{
-    products.push({'title':req.body.title});
+    const product=new Product(req.body.title);
+    product.save();
     res.redirect("/");
 };
 
 exports.getProducts=(req,res,next) => {
-    res.render("shop",{prods:products, path:'/',pageTitle:'Shop'});
+    Product.fetchAll((products)=>{
+        res.render("shop",{prods:products, path:'/',pageTitle:'Shop'});
+    });
 };
