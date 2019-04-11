35461a63bc4c79b3be7366488b6888f881edaa14
Kunal Indoliya <kunalindoliya@gmail.com>
2019-04-12 01:29:43 +0530

    Fetching single product details.

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,12 @@ exports.getProducts=(req,res,next) => {
         res.render("shop/product-list",{prods:products, path:'/products',pageTitle:'All Products'});
     });
 };
+exports.getProduct=(req,res,next)=> {
+  const id=req.params.id;
+  Product.findById(id,product =>{
+     res.render('shop/product-detail',{product:product,pageTitle:'Product Detail',path:'/products'})
+  });
+};
 exports.getIndex=(req,res,next) =>{
     Product.fetchAll((products)=>{
         res.render("shop/index",{prods:products, path:'/',pageTitle:'Shop'});
@@ -11,6 +17,16 @@ exports.getIndex=(req,res,next) =>{
 };
 exports.getCart=(req,res,next) =>{
  res.render('shop/cart',{path:'/cart',pageTitle:'Your Cart'});
+};
+
+exports.postCart=(req,res,next)=> {
+    const  id=req.body.id;
+    console.log(id);
+    res.redirect('/cart');
+};
+
+exports.getOrders=(req,res,next)=>{
+  res.render('shop/orders',{path:'/orders',pageTitle:'Orders'});
 };
 exports.getCheckout=(req,res,next)=>{
   res.render('/checkout',{path:'/checkout'});
